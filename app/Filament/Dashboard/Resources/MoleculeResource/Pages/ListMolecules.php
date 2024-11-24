fa496c137f499d5a503b2cbaf03d299d36d72149
Sagar <sriramkanakam87@gmail.com>
2024-11-24 19:20:28 +0100

    fix: IDs are now put in a subquery to reduce browser hangups

--- app/Filament/Dashboard/Resources/MoleculeResource/Pages/ListMolecules.php
+++ app/Filament/Dashboard/Resources/MoleculeResource/Pages/ListMolecules.php
@@ -11,6 +11,7 @@ use Filament\Actions\Action;
 use Filament\Forms\Components\TextInput;
 use Filament\Resources\Pages\ListRecords;
 use Illuminate\Contracts\Pagination\CursorPaginator;
+use Illuminate\Contracts\Pagination\Paginator;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Facades\DB;
 
@@ -21,6 +22,7 @@ class ListMolecules extends ListRecords
     protected static string $resource = MoleculeResource::class;
 
     public $searchResults = null;
+    public $substructureSmiles = null;
 
     protected function getHeaderActions(): array
     {
@@ -38,12 +40,13 @@ class ListMolecules extends ListRecords
                 ->button()
                 ->modalSubmitActionLabel('Search')
                 ->action(function (array $data): void {
-                    $smiles = $data['structure-smiles'];
-                    $this->searchResults = collect(DB::select("SELECT id 
-                                                            FROM mols 
-                                                            WHERE m@> mol_from_smiles('{$smiles}')::mol 
-                                                            ORDER BY tanimoto_sml(morganbv_fp(mol_from_smiles('{$smiles}')::mol), morganbv_fp(m::mol)) DESC
-                                                            ;"))->pluck('id');
+                    $this->substructureSmiles = $data['structure-smiles'];
+                    // $smiles = $data['structure-smiles'];
+                    // $this->searchResults = collect(DB::select("SELECT id 
+                    //                                         FROM mols 
+                    //                                         WHERE m@> mol_from_smiles('{$smiles}')::mol 
+                    //                                         ORDER BY tanimoto_sml(morganbv_fp(mol_from_smiles('{$smiles}')::mol), morganbv_fp(m::mol)) DESC
+                    //                                         ;"))->pluck('id')->toArray();
                     $this->getTableQuery();
                 }),
             Actions\CreateAction::make(),
@@ -52,16 +55,39 @@ class ListMolecules extends ListRecords
 
     protected function getTableQuery(): Builder
     {
-        if ($this->searchResults) {
-            return Molecule::query()
-                ->select('id', 'name', 'identifier', 'canonical_smiles', 'standard_inchi', 'status', 'active', 'synonyms')
-                ->whereIntegerInRaw('id', $this->searchResults)
-                ->with(['properties' => function ($query) {
-                    $query->select('exact_molecular_weight', 'np_likeness');
-                }]);
-        } else {
-            return Molecule::query();
+        if ($this->substructureSmiles) {
+            $query = Molecule::query()
+            ->select('id', 'name', 'identifier', 'canonical_smiles', 'standard_inchi', 'status', 'active', 'synonyms');
+
+        if ($this->substructureSmiles) {
+            $query->whereIn('id', function($query) {
+                $query->select('id')
+                      ->from('mols')
+                      ->whereRaw('m @> mol_from_smiles(?)::mol', [$this->substructureSmiles])
+                      ->orderByRaw('tanimoto_sml(morganbv_fp(mol_from_smiles(?)::mol), morganbv_fp(m::mol)) DESC', 
+                          [$this->substructureSmiles]);
+            });
         }
+
+        return $query->with(['properties' => function ($query) {
+            $query->select('molecule_id', 'exact_molecular_weight', 'np_likeness');
+        }]);
+        }
+        // if ($this->searchResults) {
+        //     return Molecule::query()
+        //         ->select('id', 'name', 'identifier', 'canonical_smiles', 'standard_inchi', 'status', 'active', 'synonyms')
+        //         ->whereIntegerInRaw('id', $this->searchResults)
+        //         ->with(['properties' => function ($query) {
+        //             $query->select('exact_molecular_weight', 'np_likeness');
+        //         }]);
+        // } 
+        return Molecule::query();
+
+    }
+
+    protected function paginateTableQuery(Builder $query): Paginator
+    {
+        return parent::paginateTableQuery($query);
     }
 
     public function getPresetViews(): array
